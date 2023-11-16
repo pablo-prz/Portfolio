c19daf91b24c254cdfbd72deba733e0ff7a9d1a1
pablo perez <perezpablo0903mail.com>
2023-11-16 17:37:39 -0300

    HB menu fix

--- src/componentes/navBar/Nav.jsx
+++ src/componentes/navBar/Nav.jsx
@@ -10,42 +10,40 @@ const Nav = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <>
-      <div className={styles.navContainer}>
-        <button onClick={handleOpen} className={styles.BurguerButton}>
-          <svg
-            className={styles.svg}
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
-            />
-          </svg>
-        </button>
-        <div className={`${styles.bar}  ${isOpen ? styles.isactive : ""}`}>
-          <a href="#Home" className={styles.buttons}>
-            {translatedTexts.navHome}
-          </a>
-          <a href="#About" className={styles.buttons}>
-            {translatedTexts.navAbout}
-          </a>
-          <a href="#Technologies" className={styles.buttons}>
-            {translatedTexts.navTech}
-          </a>
-          <a href="#Proyects" className={styles.buttons}>
-            {translatedTexts.navProjects}
-          </a>
-          <a href="#Contacts" className={styles.buttons}>
-            {translatedTexts.navContact}
-          </a>
-        </div>
+    <div className={styles.navContainer}>
+      <button onClick={handleOpen} className={styles.BurguerButton}>
+        <svg
+          className={styles.svg}
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+          />
+        </svg>
+      </button>
+      <div className={`${styles.bar}  ${isOpen ? styles.isactive : ""}`}>
+        <a href="#Home" className={styles.buttons}>
+          {translatedTexts.navHome}
+        </a>
+        <a href="#About" className={styles.buttons}>
+          {translatedTexts.navAbout}
+        </a>
+        <a href="#Technologies" className={styles.buttons}>
+          {translatedTexts.navTech}
+        </a>
+        <a href="#Proyects" className={styles.buttons}>
+          {translatedTexts.navProjects}
+        </a>
+        <a href="#Contacts" className={styles.buttons}>
+          {translatedTexts.navContact}
+        </a>
       </div>
-    </>
+    </div>
   );
 };
 export default Nav;
